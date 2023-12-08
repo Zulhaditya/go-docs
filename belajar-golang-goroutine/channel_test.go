fd33d2b50cc43767cff964bb72a1ac509aed9aaa
Zulhaditya <zulhaditya@gmail.com>
2023-12-08 22:34:59 +0700

    UPDATE : Belajar range channel di Go

--- belajar-golang-goroutine/channel_test.go
+++ belajar-golang-goroutine/channel_test.go
@@ -2,6 +2,7 @@ package belajarg_golang_goroutine
 
 import (
 	"fmt"
+	"strconv"
 	"testing"
 	"time"
 )
@@ -86,6 +87,23 @@ func TestBufferedChannel(t *testing.T) {
 	}()
 
 	time.Sleep(2 * time.Second)
+
+	fmt.Println("Selesai")
+}
+
+func TestRangeChannel(t *testing.T) {
+	channel := make(chan string)
+
+	go func() {
+		for i := 0; i < 10; i++ {
+			channel <- "Perulangan ke " + strconv.Itoa(i)
+		}
+		close(channel)
+	}()
+
+	for data := range channel {
+		fmt.Println("Menerima data", data)
+	}
 
 	fmt.Println("Selesai")
 }
