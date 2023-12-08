b340058c43a9d2bbaebbb7ed85537da702458f86
Zulhaditya <zulhaditya@gmail.com>
2023-12-08 22:43:17 +0700

    UPDATE : Belajar select channel di Go

--- belajar-golang-goroutine/channel_test.go
+++ belajar-golang-goroutine/channel_test.go
@@ -107,3 +107,30 @@ func TestRangeChannel(t *testing.T) {
 
 	fmt.Println("Selesai")
 }
+
+// select channel
+func TestSelectChannel(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+	defer close(channel1)
+	defer close(channel2)
+
+	go GiveMeResponse(channel1)
+	go GiveMeResponse(channel2)
+
+	counter := 0
+	for {
+		select {
+		case data := <-channel1:
+			fmt.Println("Data dari channel 1", data)
+			counter++
+		case data := <-channel2:
+			fmt.Println("Data dari channel 2", data)
+			counter++
+		}
+
+		if counter == 2 {
+			break
+		}
+	}
+}
