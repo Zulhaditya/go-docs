a6114df8a529eac30431a5e26c5c6b0bccecdc0a
Zulhaditya <zulhaditya@gmail.com>
2023-12-08 13:54:39 +0700

    UPDATE : Belajar buffered channel di Go

--- belajar-golang-goroutine/channel_test.go
+++ belajar-golang-goroutine/channel_test.go
@@ -6,7 +6,7 @@ import (
 	"time"
 )
 
-// jalankan: go test -v -run=TestCreateChannel
+// jalankan: go test -v -run=NamaFunctionTesting
 func TestCreateChannel(t *testing.T) {
 	/*
 	   - channel bisa digunakan untuk mengirim dan menerima data
@@ -28,5 +28,64 @@ func TestCreateChannel(t *testing.T) {
 	fmt.Println(data)
 
 	time.Sleep(5 * time.Second)
+}
 
+func GiveMeResponse(channel chan string) {
+	time.Sleep(2 * time.Second)
+	channel <- "Inayah Wulandari"
+}
+
+func TestChannelAsParameter(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+
+	go GiveMeResponse(channel)
+
+	data := <-channel
+	fmt.Println(data)
+
+	time.Sleep(3 * time.Second)
+}
+
+// channel hanya untuk mengirim data
+func OnlyIn(channel chan<- string) {
+	time.Sleep(2 * time.Second)
+	channel <- "Inayah Wulandari"
+}
+
+// channel hanya untuk menerima data
+func OnlyOut(channel <-chan string) {
+	data := <-channel
+	fmt.Println(data)
+}
+
+func TestInOutChannel(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+
+	go OnlyIn(channel)
+	go OnlyOut(channel)
+
+	time.Sleep(3 * time.Second)
+}
+
+func TestBufferedChannel(t *testing.T) {
+	channel := make(chan string, 3)
+	defer close(channel)
+
+	go func() {
+		channel <- "Ackxle"
+		channel <- "Zulhaditya"
+		channel <- "Inayah"
+	}()
+
+	go func() {
+		fmt.Println(<-channel)
+		fmt.Println(<-channel)
+		fmt.Println(<-channel)
+	}()
+
+	time.Sleep(2 * time.Second)
+
+	fmt.Println("Selesai")
 }
