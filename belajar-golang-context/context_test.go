9a7a53b913e6e5e499a9c7806c9741c403a6288e
Zulhaditya <zulhaditya@gmail.com>
2023-12-10 20:16:55 +0700

    UPDATE : Belajar context with value di Go

--- belajar-golang-context/context_test.go
+++ belajar-golang-context/context_test.go
@@ -12,3 +12,30 @@ func TestContext(t *testing.T) {
 	todo := context.TODO()
 	fmt.Println(todo)
 }
+
+func TestContextWithValue(t *testing.T) {
+	contextA := context.Background()
+
+	contextB := context.WithValue(contextA, "b", "B")
+	contextC := context.WithValue(contextA, "c", "C")
+
+	contextD := context.WithValue(contextB, "d", "D")
+	contextE := context.WithValue(contextB, "e", "E")
+
+	contextF := context.WithValue(contextC, "f", "F")
+	contextG := context.WithValue(contextF, "g", "G")
+
+	fmt.Println(contextA)
+	fmt.Println(contextB)
+	fmt.Println(contextC)
+	fmt.Println(contextD)
+	fmt.Println(contextE)
+	fmt.Println(contextF)
+	fmt.Println(contextG)
+
+	// dapatkan value context
+	fmt.Println(contextF.Value("f")) // dapat
+	fmt.Println(contextF.Value("c")) // dapat milik parent
+	fmt.Println(contextF.Value("b")) // tidak dapat, beda parent
+	fmt.Println(contextA.Value("b")) // tidak bisa mengambil data child
+}
