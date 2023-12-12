ae17390acab9d4da09f297d6008a1a5fd0a49d34
Zulhaditya <zulhaditya@gmail.com>
2023-12-13 00:08:36 +0700

    UPDATE : Belajar penanganan sql injection di Go

--- belajar-golang-database/sql_test.go
+++ belajar-golang-database/sql_test.go
@@ -96,3 +96,34 @@ func TestQuerySQLComplex(t *testing.T) {
 		fmt.Println("Created At:", createdAt)
 	}
 }
+
+func TestSQLInjection(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	username := "admin'; #"
+	password := "salah"
+
+	script := "SELECT username FROM user WHERE username = '" + username +
+		"' AND password = '" + password + "' LIMIT 1"
+	fmt.Println(script)
+	rows, err := db.QueryContext(ctx, script)
+	if err != nil {
+		panic(err)
+	}
+
+	defer rows.Close()
+
+	if rows.Next() {
+		var username string
+		err := rows.Scan(&username)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("Success login", username)
+	} else {
+		fmt.Println("Failed to login")
+	}
+}
