7eea45fc6104603e37abcccafa21840e85349499
Zulhaditya <zulhaditya@gmail.com>
2023-12-13 00:17:59 +0700

    UPDATE : Belajar sql dengan parameter di Go

--- belajar-golang-database/sql_test.go
+++ belajar-golang-database/sql_test.go
@@ -103,7 +103,7 @@ func TestSQLInjection(t *testing.T) {
 
 	ctx := context.Background()
 
-	username := "admin'; #"
+	username := "admin'; #" // script sql injection
 	password := "salah"
 
 	script := "SELECT username FROM user WHERE username = '" + username +
@@ -126,4 +126,52 @@ func TestSQLInjection(t *testing.T) {
 	} else {
 		fmt.Println("Failed to login")
 	}
+}
+
+func TestSQLInjectionSafe(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	username := "admin'; #" // script sql injection
+	password := "salah"
+
+	script := "SELECT username FROM user WHERE username = ? AND password = ? LIMIT 1"
+	fmt.Println(script)
+	rows, err := db.QueryContext(ctx, script, username, password)
+	if err != nil {
+		panic(err)
+	}
+
+	defer rows.Close()
+
+	if rows.Next() {
+		var username string
+		err := rows.Scan(&username)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("Success login", username)
+	} else {
+		fmt.Println("Failed to login")
+	}
+}
+
+func TestExecSQLParameter(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	username := "ackxle'; DROP TABLE user; #"
+	password := "ackxle"
+
+	script := "INSERT INTO user(username, password) VALUES(?,?)"
+	_, err := db.ExecContext(ctx, script, username, password)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Success insert new user")
 }
