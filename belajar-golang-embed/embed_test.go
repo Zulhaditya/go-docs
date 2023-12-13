3425c23a767cf87daddf694bdb3da531a6878204
Zulhaditya <zulhaditya@gmail.com>
2023-12-13 20:06:32 +0700

    UPDATE : Belajar embed []byte di Go

--- belajar-golang-embed/embed_test.go
+++ belajar-golang-embed/embed_test.go
@@ -3,6 +3,8 @@ package belajargolangembed
 import (
 	_ "embed"
 	"fmt"
+	"io/fs"
+	"os"
 	"testing"
 )
 
@@ -11,4 +13,14 @@ var version string
 
 func TestString(t *testing.T) {
 	fmt.Println(version)
+}
+
+//go:embed cat.png
+var catImage []byte
+
+func TestByte(t *testing.T) {
+	err := os.WriteFile("cat_new.png", catImage, fs.ModePerm)
+	if err != nil {
+		panic(err)
+	}
 }
