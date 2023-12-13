7472bfdd813a77f07ffb7355fb8072f49280b464
Zulhaditya <zulhaditya@gmail.com>
2023-12-13 20:13:30 +0700

    UPDATE : Belajar embed multiple files di Go

--- belajar-golang-embed/embed_test.go
+++ belajar-golang-embed/embed_test.go
@@ -1,7 +1,7 @@
 package belajargolangembed
 
 import (
-	_ "embed"
+	"embed"
 	"fmt"
 	"io/fs"
 	"os"
@@ -23,4 +23,20 @@ func TestByte(t *testing.T) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+//go:embed files/a.txt
+//go:embed files/b.txt
+//go:embed files/c.txt
+var files embed.FS
+
+func TestMultipleFiles(t *testing.T) {
+	a, _ := files.ReadFile("files/a.txt")
+	fmt.Println(string(a))
+
+	b, _ := files.ReadFile("files/b.txt")
+	fmt.Println(string(b))
+
+	c, _ := files.ReadFile("files/c.txt")
+	fmt.Println(string(c))
 }
