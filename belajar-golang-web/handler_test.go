20fda11d53e47b12edabe23d587e7684214e7ee6
Zulhaditya <zulhaditya@gmail.com>
2023-12-14 20:44:13 +0700

    UPDATE : Belajar handler web di Go

---
+++ belajar-golang-web/handler_test.go
@@ -0,0 +1,25 @@
+package belajar_golang_web
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+func TestHandler(t *testing.T) {
+
+	var handler http.HandlerFunc = func(writer http.ResponseWriter, request *http.Request) {
+		// logic web
+		fmt.Fprint(writer, "Server is running...")
+	}
+
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: handler,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
