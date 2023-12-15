9d501a6c5177a0c9487f7bf9a02cc11ccb9b3acf
Zulhaditya <zulhaditya@gmail.com>
2023-12-15 20:16:58 +0700

    UPDATE : Belajar servemux di Go

--- belajar-golang-web/handler_test.go
+++ belajar-golang-web/handler_test.go
@@ -23,3 +23,32 @@ func TestHandler(t *testing.T) {
 		panic(err)
 	}
 }
+
+func TestServeMux(t *testing.T) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, "Hello World!")
+	})
+
+	mux.HandleFunc("/hi", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, "Hi.")
+	})
+
+	mux.HandleFunc("/images/", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, "Images")
+	})
+
+	mux.HandleFunc("/images/thumbnails/", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, "Thumbnails")
+	})
+
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: mux,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
