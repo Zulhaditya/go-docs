ac5390c77ec033c27077ac1b6b78157bd1f4c31f
Zulhaditya <zulhaditya@gmail.com>
2023-12-22 08:23:17 +0700

    UPDATE : Inisialisasi project http-router

---
+++ belajar-golang-httprouter/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+)
+
+func main() {
+	router := httprouter.New()
+	router.GET("/", func(writer http.ResponseWriter, request *http.Request, _ httprouter.Params) {
+		fmt.Fprint(writer, "Hello HttpRouter")
+	})
+
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: router,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
