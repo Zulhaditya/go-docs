b470123ba39ce4b4c11206c70ed88be8c130e534
Zulhaditya <zulhaditya@gmail.com>
2023-12-29 16:28:34 +0700

    UPDATE : Implementasi error handling di dependency

--- belajar-golang-dependency-injection/simple/injector.go
+++ belajar-golang-dependency-injection/simple/injector.go
@@ -7,9 +7,9 @@ import (
 	"github.com/google/wire"
 )
 
-func InitializedService() *SimpleService {
+func InitializedService() (*SimpleService, error) {
 	wire.Build(
 		NewSimpleRepository, NewSimpleService,
 	)
-	return nil
+	return nil, nil
 }
