cac25ada9dc07c6f925f6510794cc7489ae84bf2
Zulhaditya <zulhaditya@gmail.com>
2023-12-29 21:17:14 +0700

    UPDATE : Implementasi provider set dan grouping

--- belajar-golang-dependency-injection/simple/injector.go
+++ belajar-golang-dependency-injection/simple/injector.go
@@ -18,3 +18,12 @@ func InitializedDatabaseRepository() *DatabaseRepository {
 	wire.Build(NewDatabaseMongoDB, NewDatabasePostgreSQL, NewDatabaseRepository)
 	return nil
 }
+
+// grouping dependency injection
+var fooSet = wire.NewSet(NewFooRepository, NewFooService)
+var barSet = wire.NewSet(NewBarRepository, NewBarService)
+
+func InitializedFooBarService() *FooBarService {
+	wire.Build(fooSet, barSet, NewFooBarService)
+	return nil
+}
