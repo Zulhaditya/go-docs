e82cfba65ff342eda1d5d73299d340e5ab1a8f15
Zulhaditya <zulhaditya@gmail.com>
2023-12-30 14:10:14 +0700

    UPDATE : Implementasi dependency injection di RESTful API

---
+++ belajar-golang-dependency-injection/injector.go
@@ -0,0 +1,39 @@
+//go:build wireinject
+// +build wireinject
+
+package main
+
+import (
+	"belajar_golang_restful_api/app"
+	"belajar_golang_restful_api/controller"
+	"belajar_golang_restful_api/middleware"
+	"belajar_golang_restful_api/repository"
+	"belajar_golang_restful_api/service"
+	"net/http"
+
+	"github.com/go-playground/validator"
+	"github.com/google/wire"
+	"github.com/julienschmidt/httprouter"
+)
+
+var categorySet = wire.NewSet(
+	repository.NewCategoryRepository,
+	wire.Bind(new(repository.CategoryRepository), new(*repository.CategoryRepositoryImpl)),
+	service.NewCategoryService,
+	wire.Bind(new(service.CategoryService), new(*service.CategoryServiceImpl)),
+	controller.NewCategoryController,
+	wire.Bind(new(controller.Category), new(*controller.CategoryControllerImpl)),
+)
+
+func InitializedServer() *http.Server {
+	wire.Build(
+		app.NewDB,
+		validator.New,
+		categorySet,
+		app.NewRouter,
+		wire.Bind(new(http.Handler), new(*httprouter.Router)),
+		middleware.NewAuthMiddleware,
+		NewServer,
+	)
+	return nil
+}
