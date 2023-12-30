db367e7dac17a0a23d31c1d5f3b6400c6c4545d2
Zulhaditya <zulhaditya@gmail.com>
2023-12-31 01:30:28 +0700

    UPDATE : Implementasi logging level di logrus

--- belajar-golang-logging/logger_test.go
+++ belajar-golang-logging/logger_test.go
@@ -12,3 +12,16 @@ func TestLogger(t *testing.T) {
 	logger.Println("Hello logger!")
 	fmt.Println("Hello logger with fmt.")
 }
+
+func TestLevel(t *testing.T) {
+	// logging level default adalah info ke atas
+	// untuk mengubah logging level gunakan logger.SetLevel()
+	logger := logrus.New()
+	logger.SetLevel(logrus.TraceLevel) // logging dimulai dari trace level
+
+	logger.Trace("Logging in level Trace")
+	logger.Debug("Logging in level Debug")
+	logger.Info("Logging in level Info")
+	logger.Warn("Logging in level Warn")
+	logger.Error("Logging in level Error")
+}
