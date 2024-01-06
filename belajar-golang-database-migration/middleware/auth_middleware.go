042d503ade395db2b4f9c8dd850f87349267b13d
Zulhaditya <zulhaditya@gmail.com>
2024-01-07 00:23:08 +0700

    UPDATE : Inisialisasi project database migration

---
+++ belajar-golang-database-migration/middleware/auth_middleware.go
@@ -0,0 +1,33 @@
+package middleware
+
+import (
+	"belajar_golang_restful_api/helper"
+	"belajar_golang_restful_api/model/web"
+	"net/http"
+)
+
+type AuthMiddleware struct {
+	Handler http.Handler
+}
+
+func NewAuthMiddleware(handler http.Handler) *AuthMiddleware {
+	return &AuthMiddleware{Handler: handler}
+}
+
+func (middleware *AuthMiddleware) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	if "SECRET" == request.Header.Get("X-API-Key") {
+		// ok
+		middleware.Handler.ServeHTTP(writer, request)
+	} else {
+		// error
+		writer.Header().Set("Content-Type", "application/json")
+		writer.WriteHeader(http.StatusBadRequest)
+
+		webResponse := web.WebResponse{
+			Code:   http.StatusUnauthorized,
+			Status: "UNAUTHORIZED",
+		}
+
+		helper.WriteToResponseBody(writer, webResponse)
+	}
+}
