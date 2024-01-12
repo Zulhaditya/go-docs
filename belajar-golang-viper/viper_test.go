3b0df4934ce37aab2570d331994b9c01b8584acd
Zulhaditya <zulhaditya@gmail.com>
2024-01-12 16:43:22 +0700

    UPDATE : Implementasi konfigurasi file YAML dan ENV di viper

--- belajar-golang-viper/viper_test.go
+++ belajar-golang-viper/viper_test.go
@@ -29,3 +29,34 @@ func TestJSON(t *testing.T) {
 	assert.Equal(t, 3306, config.GetInt("database.port"))
 	assert.Equal(t, true, config.GetBool("database.show_sql"))
 }
+
+func TestYAML(t *testing.T) {
+	config := viper.New()
+	config.SetConfigFile("config.yaml")
+	config.AddConfigPath(".")
+
+	// membaca config
+	err := config.ReadInConfig()
+	assert.Nil(t, err)
+
+	assert.Equal(t, "belajar-golang-viper", config.GetString("app.name"))
+	assert.Equal(t, "Zulhaditya", config.GetString("app.author"))
+	assert.Equal(t, "localhost", config.GetString("database.host"))
+	assert.Equal(t, 3306, config.GetInt("database.port"))
+	assert.Equal(t, true, config.GetBool("database.show_sql"))
+}
+
+func TestENV(t *testing.T) {
+	config := viper.New()
+	config.SetConfigFile("config.env")
+	config.AddConfigPath(".")
+
+	// membaca config
+	err := config.ReadInConfig()
+	assert.Nil(t, err)
+
+	assert.Equal(t, "belajar-golang-viper", config.GetString("APP_NAME"))
+	assert.Equal(t, "Zulhaditya", config.GetString("APP_AUTHOR"))
+	assert.Equal(t, 3306, config.GetInt("DATABASE_PORT"))
+	assert.Equal(t, true, config.GetBool("DATABASE_SHOW_SQL"))
+}
