de544a11fe5055f0a37663e6457da2757231faff
Zulhaditya <zulhaditya@gmail.com>
2024-01-13 17:05:57 +0700

    UPDATE : Implementasi fiber context

--- belajar-golang-fiber/routing_test.go
+++ belajar-golang-fiber/routing_test.go
@@ -9,8 +9,9 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var app = fiber.New()
+
 func TestRoutingHelloFiber(t *testing.T) {
-	app := fiber.New()
 	app.Get("/", func(ctx *fiber.Ctx) error {
 		return ctx.SendString("Hello Fiber!")
 	})
@@ -23,4 +24,30 @@ func TestRoutingHelloFiber(t *testing.T) {
 	bytes, err := io.ReadAll(response.Body)
 	assert.Nil(t, err)
 	assert.Equal(t, "Hello Fiber!", string(bytes))
+}
+
+func TestCtx(t *testing.T) {
+	app.Get("/hello", func(ctx *fiber.Ctx) error {
+		name := ctx.Query("name", "Guest")
+		return ctx.SendString("Hello " + name)
+	})
+
+	request := httptest.NewRequest("GET", "/hello?name=Ackxle", nil)
+	response, err := app.Test(request)
+	assert.Nil(t, err)
+	assert.Equal(t, 200, response.StatusCode)
+
+	bytes, err := io.ReadAll(response.Body)
+	assert.Nil(t, err)
+	assert.Equal(t, "Hello Ackxle", string(bytes))
+
+	request = httptest.NewRequest("GET", "/hello", nil)
+	response, err = app.Test(request)
+	assert.Nil(t, err)
+	assert.Equal(t, 200, response.StatusCode)
+
+	bytes, err = io.ReadAll(response.Body)
+	assert.Nil(t, err)
+	assert.Equal(t, "Hello Guest", string(bytes))
+
 }
