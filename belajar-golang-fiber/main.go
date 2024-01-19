5e679fc19f4e1ae8b3ec89ba6379e45ed24e46ea
Zulhaditya <zulhaditya@gmail.com>
2024-01-20 00:25:08 +0700

    UPDATE : Implementasi custom middleware di fiber

--- belajar-golang-fiber/main.go
+++ belajar-golang-fiber/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
@@ -11,6 +12,25 @@ func main() {
 		IdleTimeout:  time.Second * 5,
 		ReadTimeout:  time.Second * 5,
 		WriteTimeout: time.Second * 5,
+	})
+
+	app.Use(func(ctx *fiber.Ctx) error {
+		fmt.Println("i'm middleware before processing request")
+		err := ctx.Next()
+		fmt.Println("i'm middleware after processing request")
+		return err
+	})
+
+	// menjalankan middleware pada route api tertentu
+	app.Use("/api", func(ctx *fiber.Ctx) error {
+		fmt.Println("i'm middleware before processing request")
+		err := ctx.Next()
+		fmt.Println("i'm middleware after processing request")
+		return err
+	})
+
+	app.Get("/", func(ctx *fiber.Ctx) error {
+		return ctx.SendString("Hello World")
 	})
 
 	err := app.Listen("localhost:3000")
