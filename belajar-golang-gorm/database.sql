6fa318b63deef4ba4b6847e459f4c5339f16109a
Zulhaditya <zulhaditya@gmail.com>
2024-01-21 15:57:54 +0700

    UPDATE : Implementasi model convention yang sesuai

--- belajar-golang-gorm/database.sql
+++ belajar-golang-gorm/database.sql
@@ -8,3 +8,15 @@ CREATE TABLE sample
     name varchar(100) not null,
     primary key (id)
 ) ENGINE = InnoDB;
+
+CREATE TABLE users
+(
+    id varchar(100) not null,
+    password varchar(100) not null,
+    name varchar(100) not null,
+    created_at timestamp not null default current_timestamp,
+    updated_at timestamp not null default current_timestamp on update current_timestamp,
+    primary key (id)
+)ENGINE = InnoDB;
+
+SELECT * FROM users;
