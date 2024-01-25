d0cb0cf56116c399750a27011e2cbb76ccc7db0b
Zulhaditya <zulhaditya@gmail.com>
2024-01-25 22:48:34 +0700

    UPDATE : Implementasi sorted set, hash dan geo point

--- belajar-golang-redis/redis_test.go
+++ belajar-golang-redis/redis_test.go
@@ -56,3 +56,66 @@ func TestList(t *testing.T) {
 	// hapus data list
 	client.Del(ctx, "names")
 }
+
+func TestSet(t *testing.T) {
+	client.SAdd(ctx, "students", "Inayah")
+	client.SAdd(ctx, "students", "Inayah")
+	client.SAdd(ctx, "students", "Fitri")
+	client.SAdd(ctx, "students", "Fitri")
+	client.SAdd(ctx, "students", "Wulandari")
+	client.SAdd(ctx, "students", "Wulandari")
+
+	assert.Equal(t, int64(3), client.SCard(ctx, "students").Val())
+	assert.Equal(t, []string{"Inayah", "Fitri", "Wulandari"}, client.SMembers(ctx, "students").Val())
+}
+
+func TestSortedSet(t *testing.T) {
+	client.ZAdd(ctx, "scores", redis.Z{Score: 100, Member: "Inayah"})
+	client.ZAdd(ctx, "scores", redis.Z{Score: 85, Member: "Fitri"})
+	client.ZAdd(ctx, "scores", redis.Z{Score: 95, Member: "Wulandari"})
+
+	assert.Equal(t, []string{"Fitri", "Wulandari", "Inayah"}, client.ZRange(ctx, "scores", 0, 2).Val())
+	assert.Equal(t, "Inayah", client.ZPopMax(ctx, "scores").Val()[0].Member)
+	assert.Equal(t, "Wulandari", client.ZPopMax(ctx, "scores").Val()[0].Member)
+	assert.Equal(t, "Fitri", client.ZPopMax(ctx, "scores").Val()[0].Member)
+}
+
+func TestHash(t *testing.T) {
+	client.HSet(ctx, "user:1", "id", "1")
+	client.HSet(ctx, "user:1", "name", "Inayah")
+	client.HSet(ctx, "user:1", "email", "inayah@example.com")
+
+	user := client.HGetAll(ctx, "user:1").Val()
+	assert.Equal(t, "1", user["id"])
+	assert.Equal(t, "Inayah", user["name"])
+	assert.Equal(t, "inayah@example.com", user["email"])
+
+	client.Del(ctx, "user:1")
+}
+
+func TestGeoPoint(t *testing.T) {
+	client.GeoAdd(ctx, "sellers", &redis.GeoLocation{
+		Name:      "Store A",
+		Longitude: 106.818489,
+		Latitude:  -6.178966,
+	})
+
+	client.GeoAdd(ctx, "sellers", &redis.GeoLocation{
+		Name:      "Store B",
+		Longitude: 106.821568,
+		Latitude:  -6.180662,
+	})
+
+	// cari jarak store a dan store b
+	distance := client.GeoDist(ctx, "sellers", "Store A", "Store B", "km").Val()
+	assert.Equal(t, 0.3892, distance)
+
+	sellers := client.GeoSearch(ctx, "sellers", &redis.GeoSearchQuery{
+		Longitude:  106.819143,
+		Latitude:   -6.180182,
+		Radius:     5,
+		RadiusUnit: "km",
+	}).Val()
+
+	assert.Equal(t, []string{"Store A", "Store B"}, sellers)
+}
